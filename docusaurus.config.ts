0c6c3971210398867a685751fa395564dd08a7ca
Yuan Kai <kai.yuan@streamcomputing.com>
2026-01-04 11:17:48 +0800

    1.增加搜索栏。

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -107,6 +107,32 @@ const config: Config = {
     ],
   ],
 
+  themes: [
+    // ... Your other themes.
+    [
+      // 增加docusaurus-search-local本地搜索插件
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        docsRouteBasePath: ["Doc", "Tech", "UIUX", "Mgr"],
+
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        language: ["en", "zh"],
+
+        // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
+        // searchBarShortcutKeymap: "s", // Use 'S' key
+        // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
